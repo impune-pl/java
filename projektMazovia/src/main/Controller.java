a6cb350d87336a17c5b3262a76333be17a616dcb
impune-pl <wikipedia9713@gmail.com>
2017-01-27 18:02:27 +0100

    sedfrgthyuj

--- projektMazovia/src/main/Controller.java
+++ projektMazovia/src/main/Controller.java
@@ -1,4 +1,47 @@
 package main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML
+    private ListView<?> messangeList;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private TextField messengeField;
+
+    @FXML
+    private TextField ipInput;
+
+    @FXML
+    private Button connectButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        assert sendButton != null : "fx:id=\"sendButton\" was not injected: check your FXML file 'simple.fxml'.";
+        sendButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent event)
+            {
+
+                System.out.println("działa?");
+            }
+        });
+    }
+
 }
