29e9921572bc1c304c90ccfb397251cba06b59fd
impune-pl <wikipedia9713@gmail.com>
2017-05-20 16:01:47 +0200

    no changes

---
+++ NetBeansProjects/SzybakiePotegi/src/szybakiepotegi/SzybakiePotegi.java
@@ -0,0 +1,24 @@
+package szybakiepotegi;
+
+public class SzybakiePotegi 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        long a;
+        int n;
+        long wynik = 1;
+        //a do potęgi n = w
+        while(n>0)
+        {
+            if(n%2==1)
+            {
+                w*=a;
+            }
+            a*=a;
+            n/=2;
+        }
+    }
+    
+}
